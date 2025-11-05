a02a54e5f71d47b26ac4ab390f269d4a1a86d4a3
Илья Дегтярик <degtyarik.ilya@gmail.com>
2025-11-05 16:52:19 +0300

    notifications feat

--- backend/src/modules/leads/leads.module.ts
+++ backend/src/modules/leads/leads.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Lead } from '../../database/entities/lead.entity';
+import { Bot } from '../../database/entities/bot.entity';
 import { LeadsService } from './leads.service';
 import { LeadsController } from './leads.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Lead])],
+  imports: [TypeOrmModule.forFeature([Lead, Bot])],
   providers: [LeadsService],
   controllers: [LeadsController],
   exports: [LeadsService],
