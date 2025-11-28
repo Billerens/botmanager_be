02d8d8aaa137f291bcc2cc786caff0bfa50ecc44
Илья Дегтярик <degtyarik.ilya@gmail.com>
2025-11-28 13:15:56 +0300

    multifile (app) custom pages

--- backend/src/modules/custom-pages/controllers/custom-pages.controller.ts
+++ backend/src/modules/custom-pages/controllers/custom-pages.controller.ts
@@ -142,7 +142,7 @@ export class CustomPagesController {
   @UseInterceptors(
     FileInterceptor("file", {
       limits: {
-        fileSize: 50 * 1024 * 1024, // 50MB максимум
+        fileSize: 70 * 1024 * 1024, // 70MB максимум
       },
       fileFilter: (_req, file, callback) => {
         if (
