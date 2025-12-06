522a82e54c8ba6166a3ae8a777aa25e1ebfd0da4
Ilya Degtyarik <degtyarik.ilya@gmail.com>
2025-12-06 19:28:05 +0300

    browser access and public users

--- backend/src/modules/cart/cart.module.ts
+++ backend/src/modules/cart/cart.module.ts
@@ -1,13 +1,17 @@
 import { Module, forwardRef } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { JwtModule } from "@nestjs/jwt";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { Cart } from "../../database/entities/cart.entity";
 import { Product } from "../../database/entities/product.entity";
 import { Bot } from "../../database/entities/bot.entity";
 import { Message } from "../../database/entities/message.entity";
 import { ShopPromocode } from "../../database/entities/shop-promocode.entity";
+import { PublicUser } from "../../database/entities/public-user.entity";
 import { CartService } from "./cart.service";
 import { CartController } from "./cart.controller";
 import { TelegramInitDataGuard } from "../auth/guards/telegram-initdata.guard";
+import { PublicAccessGuard } from "../public-auth/guards/public-access.guard";
 import { TelegramInitDataValidationService } from "../../common/telegram-initdata-validation.service";
 import { BotsModule } from "../bots/bots.module";
 import { TelegramModule } from "../telegram/telegram.module";
@@ -17,7 +21,22 @@ import { ActivityLogModule } from "../activity-log/activity-log.module";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Cart, Product, Bot, Message, ShopPromocode]),
+    TypeOrmModule.forFeature([
+      Cart,
+      Product,
+      Bot,
+      Message,
+      ShopPromocode,
+      PublicUser,
+    ]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>("JWT_SECRET"),
+        signOptions: { expiresIn: "7d" },
+      }),
+      inject: [ConfigService],
+    }),
     forwardRef(() => BotsModule),
     TelegramModule,
     WebSocketModule,
@@ -28,6 +47,7 @@ import { ActivityLogModule } from "../activity-log/activity-log.module";
   providers: [
     CartService,
     TelegramInitDataGuard,
+    PublicAccessGuard,
     TelegramInitDataValidationService,
   ],
   exports: [CartService],
